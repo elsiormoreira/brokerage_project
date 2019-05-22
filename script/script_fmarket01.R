60658fc49ddf7a7bd9639733728556b7edd81e8c
elsiormoreira <elsiormoreira@gmail.com>
2019-05-22 10:56:49 +0100

    correct filter and concatenate code

--- script/script_fmarket01.R
+++ script/script_fmarket01.R
@@ -56,27 +56,28 @@ c12 <- word(text_clean, 12)
 
 
 # filter column c7 by values type using regex
-c7_name <- ifelse(str_detect(c7, 'd|#|^\\d'), '', c7)
-c7_obs <- ifelse(str_detect(c7, 'd|#'), c7, '')
+c7_name <- ifelse(str_detect(c7, '^d$|#|^\\d'), '', c7)
+c7_obs <- ifelse(str_detect(c7, '^d$|#|^8d?$'), c7, '')
 c7_quant <- ifelse(str_detect(c7, '^\\d\\d\\d|^\\d\\.\\d\\d\\d$'), c7, '')
 
 
 # filter c8 by values type using regex
-#c8_name <- 
-c8_obs <- ifelse(str_detect(c8, 'd|#'), c8, '')
-c8_quant <- ifelse(str_detect(c8, '^\\d\\d\\d|^\\d\\.\\d\\d\\d$'), c8, '')
+c8_name <- ifelse(str_detect(c8, '^d$|#|^\\d'), '', c8)
+c8_obs <- ifelse(str_detect(c8, '^d$|#|^8d?$'), c8, '')
+c8_quant <- ifelse(str_detect(c8, '^\\d\\d\\d$|^\\d\\.\\d\\d\\d$'), c8, '')
 c8_price <- ifelse(str_detect(c8, '^\\d?\\d\\,\\d+$'), c8, '')
 
 
 # filter c9 by values type using regex
 #c9_obs <- 
-c9_quant <- ifelse(str_detect(c9, '^\\d\\d\\d|^\\d\\.\\d\\d\\d$'), c9, '')
+c9_obs <- ifelse(str_detect(c9, '^d$|#|^8d?$'), c9, '')
+c9_quant <- ifelse(str_detect(c9, '^\\d\\d\\d$|^\\d\\.\\d\\d\\d$'), c9, '')
 c9_price <- ifelse(str_detect(c9, '^\\d?\\d\\,\\d+$'), c9, '')
 c9_total <- ifelse(str_detect(c9, '\\d\\d\\d\\,\\d'), c9, '')
 
 
 # filter c10 by values type using regex
-c10_quant <- ifelse(str_detect(c10, '^\\d\\d\\d|^\\d\\.\\d+'), c10, '')
+c10_quant <- ifelse(str_detect(c10, '^\\d\\d\\d$|^\\d\\.\\d\\d\\d$'), c10, '')
 c10_price <- ifelse(str_detect(c10, '^\\d?\\d\\,\\d+$'), c10, '')
 c10_total <- ifelse(str_detect(c10, '\\d\\d\\d\\,\\d'), c10, '')
 c10_dc <- ifelse(str_detect(c10, 'd|c'), c10, '')
@@ -88,23 +89,25 @@ c12 <- ifelse(is.na(c12), '', c12)
 
 
 # filter c11 by values type using regex
+c11_price <- ifelse(str_detect(c11, '^\\d?\\d\\,\\d+$'), c11, '')
 c11_total <- ifelse(str_detect(c11, '\\d\\d\\d\\,\\d'), c11, '')
 c11_dc <- ifelse(str_detect(c11, 'd|c'), c11, '')
 
 
 # filter c12 by values type using regex
+c12_total <- ifelse(str_detect(c12, '\\d\\d\\d\\,\\d'), c12, '')
 c12_dc <- ifelse(str_detect(c12, 'd|c'), c12, '')
 
 
 # concatenate the same type data string
 concat_1 <- str_c(c1, c2, c3, sep = ';') %>% 
             str_squish()
-concat_2 <- str_squish(str_c(c4, c5, c6, sep = ' '))
-concat_3 <- str_squish(str_c(c7_obs, c8_obs, sep = ' '))
-concat_4 <- str_remove(str_squish(str_c(c7_quant, c8_quant, c9_quant, sep = ' ')),'\\.')
-concat_5 <- str_remove(str_squish(str_c(c8_price, c9_price, c10_price, sep = ' ')), '\\.') %>% 
+concat_2 <- str_squish(str_c(c4, c5, c6, c7_name, c8_name, sep = ' '))
+concat_3 <- str_squish(str_c(c7_obs, c8_obs, c9_obs, sep = ' '))
+concat_4 <- str_remove(str_squish(str_c(c7_quant, c8_quant, c9_quant, c10_quant, sep = ' ')), '\\.')
+concat_5 <- str_remove(str_squish(str_c(c8_price, c9_price, c10_price, c11_price, sep = ' ')), '\\.') %>% 
             str_replace('\\,', '\\.')
-concat_6 <- str_remove(str_squish(str_c(c9_total, c10_total, c11_total, sep = ' ')), '\\.') %>% 
+concat_6 <- str_remove(str_squish(str_c(c9_total, c10_total, c11_total, c12_total, sep = ' ')), '\\.') %>% 
             str_replace('\\,', '\\.')
 concat_7 <- str_squish(str_c(c10_dc, c11_dc, c12_dc, sep = ' '))
 
