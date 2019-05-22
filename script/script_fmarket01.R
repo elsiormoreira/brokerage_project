9412025fa7cf27b735202abd522978536643db54
elsiormoreira <elsiormoreira@gmail.com>
2019-05-22 09:01:57 +0100

    rectify dates extraction code

--- script/script_fmarket01.R
+++ script/script_fmarket01.R
@@ -23,7 +23,8 @@ dates <- text_raw %>%
         unlist() %>% 
         as.vector() %>% 
         str_squish() %>% 
-        str_subset('\\d{4}\\s\\d{1}\\s\\d{2}\\/\\d{2}\\/\\d{4}$')
+        str_subset('\\d{4}\\s\\d{1}\\s\\d{2}\\/\\d{2}\\/\\d{4}$') %>% 
+        str_remove_all('\\d+\\s\\d{1}\\s')
         
 
 # basic clean data for extraction
